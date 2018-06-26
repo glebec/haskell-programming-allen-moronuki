5ff86d2720d8fc34bd9f6233cc34391eb2c0be52
Gabriel Lebec <glebec@gmail.com>
2018-06-25 22:58:55 -0400

    feat(ch. 13): do notation experiments

--- 13-building-projects/13.00-scratch.hs
+++ 13-building-projects/13.00-scratch.hs
@@ -1,2 +1,18 @@
 module Scratch13 where
 
+twoo :: IO Bool
+twoo = do c  <- getLine
+          c' <- getLine
+          return (c == c')
+
+-- main :: IO ()
+-- main = do
+--     b <- twoo
+--     if b then putStrLn "Yup same"
+--          else putStrLn "Different"
+
+main = twoo >>= \p -> putStrLn $ if' p "Yup same" "Different"
+
+if' :: Bool -> a -> a -> a
+if' True  a _ = a
+if' False _ b = b
