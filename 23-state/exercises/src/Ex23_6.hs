ca8c1e98bbd65059a4648caa9210060a33c3e9cf
Gabriel Lebec <glebec@gmail.com>
2018-09-27 12:13:33 -0400

    feat(ch. 23): through 23.6

---
+++ 23-state/exercises/src/Ex23_6.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE InstanceSigs #-}
+
+module Ex23_6 where
+
+newtype Moi s a = Moi { runMoi :: s -> (a, s) }
+
+instance Functor (Moi s) where
+    fmap f (Moi s2as) = Moi $ \s -> let (a, s') = s2as s in (f a, s')
+
+instance Applicative (Moi s) where
+    pure a = Moi $ \s -> (a, s)
+    (<*>) (Moi s2fs) (Moi s2xs) = Moi $
+        \s -> let (f, s1) = s2fs s
+                  (x, s2) = s2xs s1
+              in (f x, s2)
+
+instance Monad (Moi s) where
+    (>>=) :: Moi s a -> (a -> Moi s b) -> Moi s b
+    (>>=) (Moi s2as) a2Msb = Moi $
+        \s -> let (a, s')  = s2as s
+                  (b, s'') = runMoi (a2Msb a) s'
+              in  (b, s'')
--- 23-state/exercises/src/Main.hs
+++ 23-state/exercises/src/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Ex23_5
+import Ex23_6
 
 main :: IO ()
 main = do
