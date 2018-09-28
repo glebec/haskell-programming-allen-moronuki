45f3561763ff6872024d064c5157772b1bf6995a
Gabriel Lebec <glebec@gmail.com>
2018-09-28 15:06:32 -0400

    feat(ch. 24): through 24.4

---
+++ 24-parser-combinators/exercises/src/Ex24_4.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Ex24_4 where
+
+import Control.Applicative
+import Data.Ratio ((%))
+import Text.Trifecta
+
+badFraction = "1/0"
+alsoBad = "10"
+shouldWork = "1/2"
+shouldAlsoWork = "2/1"
+
+parseFraction :: Parser Rational
+parseFraction = do
+    numerator <- decimal
+    char '/'
+    denominator <- decimal
+    case denominator of
+        0 -> fail "Denominator cannot be zero"
+        _ -> return (numerator % denominator)
+
+demoFraction :: IO ()
+demoFraction = do
+    let parseFraction' = parseString parseFraction mempty
+    print $ parseFraction' shouldWork
+    print $ parseFraction' shouldAlsoWork
+    print $ parseFraction' badFraction
+    print $ parseFraction' alsoBad
+
+parseIntEof :: Parser Integer
+parseIntEof = integer >>= \i -> eof >> return i
+
+demoIntEof :: IO ()
+demoIntEof = print $ parseString parseIntEof mempty "123"
--- 24-parser-combinators/exercises/src/Main.hs
+++ 24-parser-combinators/exercises/src/Main.hs
@@ -2,7 +2,11 @@ module Main where
 
 import LearnParsers
 import Ex24_3
+import Ex24_4
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+    demoParsers
+    demo24_3
+    demoFraction
+    demoIntEof
