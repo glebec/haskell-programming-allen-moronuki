6613383144afb601bf71288f139bfbac0e513b4c
Gabriel Lebec <glebec@gmail.com>
2018-10-01 17:39:35 -0400

    feat(ch. 24): semver spec (failing)

--- 24-parser-combinators/exercises/src/Ex24_11.hs
+++ 24-parser-combinators/exercises/src/Ex24_11.hs
@@ -3,6 +3,8 @@ module Ex24_11 where
 import Control.Applicative
 import Text.Trifecta
 import Text.Parser.LookAhead
+import Test.Hspec
+import Data.Foldable (sequenceA_)
 
 -- 1. Write a parser for semver as per http://semver.org. Then write an `Ord`
 -- instance for `SemVer` that obeys the spec on the site.
@@ -26,10 +28,9 @@ instance Eq SemVer where
 
 instance Ord SemVer where
     compare s@(SemVer mj mn pt rl _) s'@(SemVer mj' mn' pt' rl' _)
-        | s == s' = EQ
-        | otherwise = if (mj, mn, pt, rl) > (mj', mn', pt', rl')
-                      then GT
-                      else LT
+        | (mj, mn, pt, rl) > (mj', mn', pt', rl') = GT
+        | s == s'                                 = EQ
+        | otherwise                               = LT
 
 parseVersion :: Parser (Major, Minor, Patch)
 parseVersion = do
@@ -57,35 +58,97 @@ parseRelIdentifier = (lookAhead (noneOf "0") >> parseNOSI) <|>
                      parseNOSS
 
 parseRelease :: Parser Release
-parseRelease = do
-    char '-'
-    sepBy1 parseRelIdentifier (char '.')
+parseRelease = sepBy1 parseRelIdentifier (char '.')
 
 parseMetadata :: Parser Metadata
-parseMetadata = do
-    char '+'
-    sepBy1 parseMetaIdentifier (char '.')
+parseMetadata = sepBy1 parseMetaIdentifier (char '.')
 
 parseSemVer :: Parser SemVer
 parseSemVer = do
     (maj, min, ptc) <- parseVersion
-    rel  <- try parseRelease  <|> pure []
-    meta <- try parseMetadata <|> pure []
+    rel  <- option [] (char '-' >> parseRelease)
+    meta <- option [] (char '+' >> parseMetadata)
     return $ SemVer maj min ptc rel meta
 
-demoParseSemVer :: String -> Result SemVer
-demoParseSemVer = parseString parseSemVer mempty
+psv :: String -> Result SemVer
+psv = parseString parseSemVer mempty
 
-sv1, sv2, sv3 :: Result SemVer
-sv1 = demoParseSemVer "2.1.1"
-sv2 = demoParseSemVer "1.0.0-x.7.z.92"
-sv3 = demoParseSemVer "19.2.38-alpha.01+myver2.09"
+-- Testing
 
-svComp1, svComp2 :: Bool
-svComp1 = SemVer 2  1 1 [] []  > SemVer 2 1 0 [] []
-svComp2 = SemVer 12 3 1 [] []  > SemVer 2 1 0 [] []
-svComp3 = SemVer 1  1 1 [NOSS "alpha"] [] > SemVer 1  1 1 [NOSI 1] []
+yields :: Eq a => a -> Result a -> Bool
+yields s (Success s') = s == s'
+yields _ _ = False
 
-svComp4 :: Result Bool
-svComp4 = (>) <$> demoParseSemVer "19.2.38-alpha.10+myver3.09" <*>
-                  demoParseSemVer "19.2.38-alpha.4+myver2.09"
+pFailed :: Result a -> Bool
+pFailed (Success _) = False
+pFailed _ = True
+
+specParseYields :: String -> SemVer -> SpecWith ()
+specParseYields s sv =
+    it ("parses " ++ s ++ " as " ++ show sv) $
+        psv s `shouldSatisfy` yields sv
+
+specParseFails :: String -> SpecWith ()
+specParseFails s =
+    it ("fails to parse " ++ s) $
+        psv s `shouldSatisfy` pFailed
+
+specOp :: (SemVer -> SemVer -> Bool) -> String
+       -> String -> String -> SpecWith ()
+specOp op msg a b = it (a ++ " " ++ msg ++ " " ++ b) $
+    liftA2 op (psv a) (psv b) `shouldSatisfy` yields True
+
+specEQ, specLT :: String -> String -> SpecWith ()
+specEQ = specOp (==) "equals"
+specLT = specOp (<) "is less than"
+
+checkSemVer :: IO ()
+checkSemVer = hspec $ do
+    describe "parseSemVer" $ do
+        mapM_ (uncurry specParseYields)
+            [ ( "0.0.0",
+                SemVer 0 0 0 [] [] )
+            , ( "2.1.3",
+                SemVer 2 1 3 [] [] )
+            , ( "14.103.9",
+                SemVer 14 103 9 [] [] )
+            , ( "1.1.1-alpha",
+                SemVer 1 1 1 [NOSS "alpha"] [] )
+            , ( "1.1.1-the-coolest.1",
+                SemVer 1 1 1 [NOSS "the-coolest", NOSI 1] [] )
+            , ( "1.2.3-x.8.wow.19"
+              , SemVer 1 2 3 [NOSS "x", NOSI 8, NOSS "wow", NOSI 19] [] )
+            , ( "0.1.0+mydata",
+                SemVer 0 1 0 [] [NOSS "mydata"] )
+            , ( "0.1.0+5.my-data.000",
+                SemVer 0 1 0 [] [NOSS "5", NOSS "my-data", NOSS "000"] )
+            , ( "1.2.3-13.0.beta+xo.08.0.wow"
+              , SemVer 1 2 3
+                [NOSI 13, NOSI 0, NOSS "beta"]
+                [NOSS "xo", NOSS "08", NOSS "0", NOSS "wow"] ) ]
+        mapM_ specParseFails
+            [ "00.0.0", "0.00.0", "0.0.00"
+            , "01.0.0", "0.01.0", "0.0.01"
+            , "0.0.0-00", "0.0.0-x.00", "0.0.0-01.x"
+            , "1", "1.2", "a", "1.a", "1.2.a"
+            , "1.1.1-", "1.1.1+", "1.1.1-+", "1.1.1-+alpha" ]
+    describe "SemVer Ord" $
+        sequenceA_
+            [ "1.0.0" `specEQ` "1.0.0"
+            , "0.1.9" `specEQ` "0.1.9"
+            , "0.1.9-alpha" `specEQ` "0.1.9-alpha"
+            , "0.1.9+foo" `specEQ` "0.1.9+bar"
+            , "0.1.9-alpha+foo" `specEQ` "0.1.9-alpha+bar"
+            , "3.0.0" `specLT` "3.0.1"
+            , "3.0.0" `specLT` "13.0.0"
+            , "13.9.0" `specLT` "13.39.0"
+            , "13.9.45" `specLT` "13.9.55"
+            , "1.0.0-alpha" `specLT` "1.0.0-alpha.1"
+            , "1.0.0-alpha.1" `specLT` "1.0.0-alpha.beta"
+            , "1.0.0-beta" `specLT` "1.0.0-beta.2"
+            , "1.0.0-beta" `specLT` "1.0.0-beta.2"
+            , "1.0.0-beta.2" `specLT` "1.0.0-beta.11"
+            , "1.0.0-beta.11" `specLT` "1.0.0-rc.1"
+            , "1.0.0-rc.1" `specLT` "1.0.0"
+            , "1.0.0-rc.1+102" `specLT` "1.0.0"
+            , "1.0.0-rc.1+102" `specLT` "1.0.0+102" ]
