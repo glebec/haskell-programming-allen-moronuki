ce83996d33edba20751f3657120521cdd55d47d6
Gabriel Lebec <glebec@gmail.com>
2019-07-01 23:19:04 -0400

    chore(ch. 28): foldr -> foldr'

--- 28-basic-libraries/exercises/src/Ex28_6.hs
+++ 28-basic-libraries/exercises/src/Ex28_6.hs
@@ -3,18 +3,19 @@ module Ex28_6 where
 import Criterion.Main (defaultMain, bench, whnf)
 import qualified Data.Map.Strict as M
 import qualified Data.Set as S
+import Data.Foldable (foldr')
 
 insertUnit :: Ord a => a -> M.Map a () -> M.Map a ()
 insertUnit a = M.insert a ()
 
 mapFromList' :: [Int] -> M.Map Int ()
-mapFromList' = foldr insertUnit M.empty
+mapFromList' = foldr' insertUnit M.empty
 
 setFromList' :: [Int] -> S.Set Int
-setFromList' = foldr S.insert S.empty
+setFromList' = foldr' S.insert S.empty
 
 ex28_6 :: IO ()
 ex28_6 = defaultMain
-    [ bench "map insert" $ whnf mapFromList' [1..9999] -- 3.727 ms
-    , bench "set insert" $ whnf setFromList' [1..9999] -- 2.880 ms
+    [ bench "map insert" $ whnf mapFromList' [1..9999] -- 3.562 ms
+    , bench "set insert" $ whnf setFromList' [1..9999] -- 2.830 ms
     ]
