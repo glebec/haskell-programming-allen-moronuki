796bb9311a2dbbeec8328601d60da501add9ad3f
Gabriel Lebec <glebec@gmail.com>
2019-08-04 17:54:36 -0400

    feat(ch. 31): working TCP server

--- 31-final-project/fingerd/src/Debug.hs
+++ 31-final-project/fingerd/src/Debug.hs
@@ -1,5 +1,29 @@
-module Debug where
+module Main where
+
+import Control.Monad (forever)
+import Network.Socket hiding (recv)
+import Network.Socket.ByteString (recv, sendAll)
+
+logAndEcho :: Socket -> IO ()
+logAndEcho sock = forever $ do
+    (soc, _) <- accept sock
+    printAndKickback soc  -- NB: blocking
+    close soc
+  where
+    printAndKickback conn = do
+        msg <- recv conn 1024
+        print msg
+        sendAll conn msg
 
 main :: IO ()
-main = do
-    putStrLn "hello world"
+main = withSocketsDo $ do
+    addrInfos <- getAddrInfo
+                (Just $ defaultHints {addrFlags = [AI_PASSIVE]})
+                Nothing
+                (Just "79")  -- port
+    let serverAddr = head addrInfos
+    sock <- socket (addrFamily serverAddr) Stream defaultProtocol
+    bind sock (addrAddress serverAddr)
+    listen sock 1
+    logAndEcho sock
+    close sock
